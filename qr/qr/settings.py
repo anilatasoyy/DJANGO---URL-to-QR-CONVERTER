efe6d7f2906dc6414d97d8afa0f4005b44ad8c16
Anıl Atasoy <54770548+anilatasoyy@users.noreply.github.com>
2022-03-04 22:39:23 +0300

    Update settings.py

--- qr/qr/settings.py
+++ qr/qr/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v0oz(sk108nfju$+x$)7z23nm=x2ojr3u!ddrxex_utaf9+-7e'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
